d15f319747dc62da8592813ebdf98292b7c18485
Uliana <a_k_ula@ukr.net>
2022-10-10 21:13:24 +0300

    first commit

---
+++ AndroidWebAPIShop/Infrastructure/ShopEFContext.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class ShopEFContext : DbContext
+    {
+        public ShopEFContext(DbContextOptions<ShopEFContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CategoryEntity> Categories { get; set; }
+
+
+    }
+}
