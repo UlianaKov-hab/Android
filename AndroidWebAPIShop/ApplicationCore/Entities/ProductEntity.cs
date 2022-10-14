851b1f8dfbb62fdd6687104f1ef186e347c4438c
Uliana <a_k_ula@ukr.net>
2022-10-14 20:25:38 +0300

    Add Identity tables

---
+++ AndroidWebAPIShop/ApplicationCore/Entities/ProductEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class ProductEntity : BaseEntity<int>
+    {
+        [Required, StringLength(255)]
+        public string Name { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required, StringLength(4000)]
+        public string Description { get; set; }
+        public ProductEntity()
+        {
+
+        }
+    }
+}
--- AndroidWebAPIShop/Infrastructure/ShopEFContext.cs
+++ AndroidWebAPIShop/Infrastructure/ShopEFContext.cs
@@ -1,4 +1,7 @@
 ﻿using ApplicationCore.Entities;
+using ApplicationCore.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,13 +11,37 @@ using System.Threading.Tasks;
 
 namespace Infrastructure
 {
-    public class ShopEFContext : DbContext
+    public class ShopEFContext : IdentityDbContext<AppUser, AppRole, long, IdentityUserClaim<long>,
+        AppUserRole, IdentityUserLogin<long>,
+        IdentityRoleClaim<long>, IdentityUserToken<long>>
     {
         public ShopEFContext(DbContextOptions<ShopEFContext> options) : base(options)
         {
         }
 
         public DbSet<CategoryEntity> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<AppUserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(r => r.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(r => r.UserId)
+                    .IsRequired();
+            });
+            //builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+
 
 
     }
